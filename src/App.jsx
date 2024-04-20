6f7cf3f43905d0dc95d26b56b995fdcf35e64803
blacdav <davidaniefoik@gmail.com>
2024-04-20 09:23:38 +0100

    Updated the LastOrders components to monitor transaction status and render specific color

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className={`${theme ? 'bg-tertiary text-black' : 'bg-black text-secondary'} App pb-5 h-screen w-screen`}>
       <div className='flex h-screen w-screen pb-5'>
-          {/* <Sidebar visible={showBar} display={theme} setDisplay={setTheme} /> */}
+          <Sidebar visible={showBar} display={theme} setDisplay={setTheme} />
         <div className='w-screen'>
           <Header visible={showBar} setVisible={setShowBar} display={theme} />
           <div className='grid grid-cols-1 gap-5 lg:grid-cols-6 h-[93%] lg:h-[90%] overflow-y-scroll'>
